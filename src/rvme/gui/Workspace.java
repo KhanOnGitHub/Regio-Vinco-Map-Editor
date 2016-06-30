8a421d461cdc4f5fddd8fc43c2527a65334b80e0
eyb0s <eyb0s@E525.mshome.net>
2016-06-30 09:39:24 -0400

    Setting up the workspace witht he necessary variables

--- src/rvme/gui/Workspace.java
+++ src/rvme/gui/Workspace.java
@@ -6,10 +6,21 @@
 package rvme.gui;
 
 import java.io.IOException;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import properties_manager.PropertiesManager;
 import saf.ui.AppGUI;
 import saf.AppTemplate;
 import saf.components.AppWorkspaceComponent;
 import rvme.controller.MapEditorController;
+import rvme.data.Subregion;
 
 
 
@@ -25,9 +36,65 @@ public class Workspace extends AppWorkspaceComponent {
     
     MapEditorController mapEditorController;
     
+    //OUR SPLIT PANE
+    SplitPane mapTable;
+    
+    //THIS HAS OUR SUBREGION MAP
+    StackPane mapPane;
+    Pane subregionsPane;
+    Label sliderLabel;
+    Slider mapZoomSlider;
+    
+    //OUR REGION FOR VIEWING AND MANAGING SUBREGIONS
+    VBox subregionsBox;
+    Label subregionsLabel;
+    HBox subregionsToolbar;
+    TableView<Subregion> subregionsTable;
+    TableColumn subregionNameColumn;
+    TableColumn subregionCapitalColumn;
+    TableColumn subregionLeaderColumn;
+    
+    
     public Workspace(AppTemplate initApp) throws IOException {
         app = initApp;
         gui = app.getGUI();
+        layoutGUI();        
+    }
+    
+    private void layoutGUI() {
+        createSplitPane();
+    }
+    
+    private void setupHandlers() {
+        
+    }
+    
+    private StackPane createMapPane() {
+        PropertiesManager props = PropertiesManager.getPropertiesManager();
+        
+        //SETUP MAP PANE COMPONENTS
+        mapPane = new StackPane();
+        subregionsPane = new Pane();
+        
+        //SETUP MAP ZOOM SLIDER
+        mapZoomSlider = new Slider(0, 1, .5);
+        mapZoomSlider.setShowTickMarks(true);
+        mapZoomSlider.setMajorTickUnit(.25f);
+        mapZoomSlider.setBlockIncrement(.1f);
+        
+        sliderLabel = new Label();
+        
+        
+                
+        return null;
+    }
+    
+    private TableView createTableView() {
+        return null;
+    }
+    
+    private void createSplitPane() {
+        
     }
     
     @Override
